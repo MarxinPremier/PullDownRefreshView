e38044db25a43b3ba285b94050f19fd03f4d2915
hejie <hejie@zhixueyun.com>
2016-12-15 13:46:29 +0800

    下拉刷新控件

---
+++ SpringView/demo/src/main/java/com/liaoinstan/demospring/demo9/MyDiyFooter.java
@@ -0,0 +1,44 @@
+package com.liaoinstan.demospring.demo9;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.liaoinstan.springview.container.BaseFooter;
+
+/**
+ * Created by hejie on 2016/11/23.
+ */
+
+public class MyDiyFooter extends BaseFooter{
+
+    @Override
+    public View getView(LayoutInflater inflater, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public void onPreDrag(View rootView) {
+
+    }
+
+    @Override
+    public void onDropAnim(View rootView, int dy) {
+
+    }
+
+    @Override
+    public void onLimitDes(View rootView, boolean upORdown) {
+
+    }
+
+    @Override
+    public void onStartAnim() {
+
+    }
+
+    @Override
+    public void onFinishAnim() {
+
+    }
+}
